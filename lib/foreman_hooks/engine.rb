54a1bfb9bb81e000c8b3f345e0dd5447aab2b53c
Amos Benari <abenari@redhat.com>
2013-12-02 11:19:20 +0200

    register the plugin

--- lib/foreman_hooks/engine.rb
+++ lib/foreman_hooks/engine.rb
@@ -5,5 +5,10 @@ module ForemanHooks
     config.to_prepare do
       ForemanHooks.hooks.each { |klass,events| ForemanHooks.attach_hook(klass.constantize, events) }
     end
+
+    initializer 'foreman_hooks.register_plugin', :after=> :finisher_hook do |app|
+      Foreman::Plugin.register :foreman_hooks do
+      end if defined? Foreman::Plugin
+    end
   end
 end
