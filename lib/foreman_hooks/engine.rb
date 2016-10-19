fb9dd1d4b3f085dfc896e95dfa2093a4fc318cce
Dominic Cleal <dominic@cleal.org>
2016-10-19 14:39:58 +0100

    Log and continue app startup on unknown hook object constant

--- lib/foreman_hooks/engine.rb
+++ lib/foreman_hooks/engine.rb
@@ -3,7 +3,15 @@ require 'foreman_hooks'
 module ForemanHooks
   class Engine < ::Rails::Engine
     config.to_prepare do
-      ForemanHooks.hooks.each { |klass,events| ForemanHooks.attach_hook(klass.constantize, events) }
+      ForemanHooks.hooks.each do |klass,events|
+        begin
+          klass_const = klass.constantize
+        rescue NameError => e
+          ForemanHooks.logger.error "foreman_hooks: unknown hook object #{klass}, check against `foreman-rake hooks:objects`"
+          next
+        end
+        ForemanHooks.attach_hook(klass_const, events)
+      end
     end
 
     initializer 'foreman_hooks.register_plugin', :before => :finisher_hook do |app|
