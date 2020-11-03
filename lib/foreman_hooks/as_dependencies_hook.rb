cf48080a5003d45ff2acecc90e1742ec060ae66d
Shimon Shtein <sshtein@redhat.com>
2020-11-03 15:21:03 +0200

    Fix error caused by loading string constants

--- lib/foreman_hooks/as_dependencies_hook.rb
+++ lib/foreman_hooks/as_dependencies_hook.rb
@@ -2,6 +2,7 @@ module ForemanHooks
   module ASDependenciesHook
     def load_missing_constant(from_mod, constant_name)
       super(from_mod, constant_name).tap do |ret|
+        return ret unless ret.try(:name)
         ForemanHooks.hooks.each do |klass,events|
           ForemanHooks.attach_hook(ret, events) if ret.name == klass
         end
